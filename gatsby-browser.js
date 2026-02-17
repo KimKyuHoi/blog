f6a6f5ba8a748ededa25dfaaebb233fc621594f4
KimKyuHoi <k546kh@gmail.com>
2026-02-17 22:27:01 +0900

    feat: scroll restoration적용

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,8 @@ import './src/style.css';
 import 'prismjs/themes/prism-tomorrow.css';
 
 export { wrapRootElement } from './src/wrap-root-element';
+
+export const shouldUpdateScroll = ({ routerProps: { location } }) => {
+  if (location.hash) return false;
+  return [0, 0];
+};
